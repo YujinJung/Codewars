29bf8b9fb588929773c17b760ed0a3516971e512
YuJin.Jung <Ujin.Jung9@gmail.com>
2017-12-29 16:15:54 +0900

    5kyu john and ann sign up for codewars

---
+++ 5kyu_john_and_ann_sign_up_for_codewars.cpp
@@ -0,0 +1,57 @@
+typedef long long ll;
+
+class Johnann
+{
+public:
+    static void vectorJA(std::vector<ll> &j, std::vector<ll> &a, const int& n)
+    {
+      j.push_back(0); a.push_back(1);
+      
+      for(ll i = 1; i < n; i++)
+      {
+        j.push_back(i - a.at(j.at(i-1)));
+        a.push_back(i - j.at(a.at(i-1)));
+      }
+    }
+    static std::vector<ll> john(ll n)
+    {
+      std::vector<ll> j, a;
+      vectorJA(j, a, n);
+      return j;
+    }
+    static std::vector<long long> ann(long long n)
+    {
+      std::vector<ll> j, a;
+      vectorJA(j, a, n);
+      return a;
+    }
+    static long long sumJohn(long long n)
+    {
+      std::vector<ll> j, a;
+      j.push_back(0); a.push_back(1);
+      
+      ll result = j.at(0);
+      for(ll i = 1; i < n; i++)
+      {
+        j.push_back(i - a.at(j.at(i-1)));
+        a.push_back(i - j.at(a.at(i-1)));
+        result += i - a.at(j.at(i-1));
+      }
+      return result;
+    }
+    static long long sumAnn(long long n)
+    {
+      std::vector<ll> j, a;
+      j.push_back(0); a.push_back(1);
+      
+      ll result = a.at(0);
+      for(ll i = 1; i < n; i++)
+      {
+        j.push_back(i - a.at(j.at(i-1)));
+        a.push_back(i - j.at(a.at(i-1)));
+        result += i - j.at(a.at(i-1));
+      }
+      return result;
+    }
+};
+
